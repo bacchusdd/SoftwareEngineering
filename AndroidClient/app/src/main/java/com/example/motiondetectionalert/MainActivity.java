e19e5ed7cc3332e0d223f1d4d3cb15ff81dcb68f
foba1 <foba1@naver.com>
2021-05-26 23:39:18 +0900

    Add activities(home, register, setting).

--- AndroidClient/app/src/main/java/com/example/motiondetectionalert/MainActivity.java
+++ AndroidClient/app/src/main/java/com/example/motiondetectionalert/MainActivity.java
@@ -2,25 +2,46 @@ package com.example.motiondetectionalert;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Layout;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText EditText_id, EditText_password;
+    Button Button_join, Button_login;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        LinearLayout layout = new LinearLayout(this);
-        layout.setOrientation(LinearLayout.VERTICAL);
+        EditText_id = findViewById(R.id.EditText_id);
+        EditText_password = findViewById(R.id.EditText_password);
+        Button_join = findViewById(R.id.Button_join);
+        Button_login = findViewById(R.id.Button_login);
 
-        TextView t1 = new TextView(this);
-        t1.setTextSize(30);
-        t1.setText("Software Engineering Project");
-        layout.addView(t1);
+        Button_join.setClickable(true);
+        Button_join.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
 
-        setContentView(layout);
+        Button_login.setClickable(true);
+        Button_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String id = EditText_id.getText().toString();
+                String password = EditText_password.getText().toString();
+            }
+        });
     }
 }
