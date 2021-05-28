09b7b9c1a67260801dac5b25ffb186bd8fd7d6d4
bacchusdd <jinny3685@naver.com>
2021-05-29 01:13:53 +0900

    - gallery를 시작화면으로 - back button과 data list 보여줄 recycler view 생성

--- AndroidClient/app/src/main/java/com/example/motiondetectionalert/Gallery.java
+++ AndroidClient/app/src/main/java/com/example/motiondetectionalert/Gallery.java
@@ -3,14 +3,33 @@ package com.example.motiondetectionalert;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class Gallery extends AppCompatActivity {
+    private ImageButton back_btn;
+    private TextView albumtext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gallery);
+
+        //back버튼 누르면 이전 페이지로 돌아감
+        back_btn = (ImageButton)findViewById(R.id.back_btn);
+        back_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        //album title
+        albumtext = (TextView) findViewById(R.id.albumtext);
     }
+
+
 
     //게시판 형식으로 db에 저장된 date들 불러오기
     //GalleryDate class 사용
