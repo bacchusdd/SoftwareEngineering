e68c6b63466109553830a7197df0c5aff2804fd8
KKHYeon <46926610+KKHYeon@users.noreply.github.com>
2021-05-29 18:27:23 +0900

    Update

--- AndroidClient/app/src/main/java/com/example/motiondetectionalert/Gallery.java
+++ AndroidClient/app/src/main/java/com/example/motiondetectionalert/Gallery.java
@@ -3,20 +3,45 @@ package com.example.motiondetectionalert;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 
-public class Gallery extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Gallery extends AppCompatActivity  {
+
+    Button back_button;
+    TextView menu;
+    ListView date_list;
+    List<String> datefromdb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gallery);
+
+        this.initializeLayout();
     }
 
+    public void initializeLayout(){
+        back_button = (Button)findViewById(R.id.back_button);
+        menu = (TextView)findViewById(R.id.text_menu);
+        date_list = (ListView)findViewById(R.id.date_list);
+
+        datefromdb = new ArrayList<>();
+        datefromdb.add("2021-05-29");
+        datefromdb.add("2021-05-30");
+
+        SwitchListAdapter date_adapter = new SwitchListAdapter(this, datefromdb);
+
+        date_list.setAdapter(date_adapter);
+    }
     //게시판 형식으로 db에 저장된 date들 불러오기
     //GalleryDate class 사용
     //adapter 필요함
     public void bringDates(){
-
     }
 
     //각 날짜 누르면 db에 저장된 photo들 불러오기
@@ -25,8 +50,6 @@ public class Gallery extends AppCompatActivity {
     public void bringPhotoList(){
 
     }
-
-
 }
 
 
