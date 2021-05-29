458401c104b23cb01deca85808d83d95bc7ee41e
HanWool-Jeong <soink3667@naver.com>
2021-05-29 14:05:32 +0900

    add function checking permissions in MainActivity.java

--- AndroidClient/app/src/main/java/com/example/motiondetectionalert/MainActivity.java
+++ AndroidClient/app/src/main/java/com/example/motiondetectionalert/MainActivity.java
@@ -1,13 +1,19 @@
 package com.example.motiondetectionalert;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.text.Layout;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private final int REQUEST_CODE_PERMISSIONS = 10;
+    private final String[] REQUIRED_PERMISSIONS = new String[]{"android.permission.CAMERA", "android.permission.WRITE_EXTERNAL_STORAGE", "android.permission.INTERNET"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,4 +29,31 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(layout);
     }
+
+    // 권한 확인
+    // 이전에 권한이 확인되지 않았으면 앱 실행시 권한을 물어본다.
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                // 권한 확인시 발생 이벤트
+            } else {
+                Toast.makeText(this, "Permissions not granted by the user.", Toast.LENGTH_SHORT).show();
+                this.finish();
+            }
+        }
+    }
+
+    // 권한 확인 도와주는 유틸함수
+    // REQUIRED_PERMISSIONS 배열에서 권한이 하나라도 없으면 false를 리턴
+    private boolean allPermissionsGranted() {
+        for(String permission : REQUIRED_PERMISSIONS){
+            if(ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
