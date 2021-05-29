36478f287bc255f2d4618cb22d2271ae7f4815d9
bacchusdd <jinny3685@naver.com>
2021-05-29 21:48:32 +0900

    test by 유경 : photolist 생성, 값 넣기

---
+++ AppServer/historymanage/test.py
@@ -0,0 +1,18 @@
+import pyrebase
+
+config = {
+    "apiKey": "AIzaSyDg6YB90dDWMXuyJMFVWEPdcSGgmrzOjSE",
+    "authDomain": "motion-detection-app-39b9b.firebaseapp.com",
+    "databaseURL": "https://motion-detection-app-39b9b-default-rtdb.firebaseio.com",
+    "projectId": "motion-detection-app-39b9b",
+    "storageBucket": "motion-detection-app-39b9b.appspot.com",
+    "messagingSenderId": "630374650472",
+    "appId": "1:630374650472:web:a250121341a75fe33661dd",
+    "measurementId": "G-EZ2JHNE4CC"
+};
+
+firebase = pyrebase.initialize_app(config)
+firemain = firebase.database()
+
+
+firemain.child("photolist").child("kang").child("dates").child("date").child("2021-05-29").child("urls").update({"photo_url":"gs://motion-detection-app-39b9b.appspot.com/ex1.jpg"})
