3ebc702362b2461ff58896fd8b845fa7e5ffbf03
KKHYeon <46926610+KKHYeon@users.noreply.github.com>
2021-05-29 19:46:43 +0900

    Commit recommit Gallery.java

--- AndroidClient/app/src/main/java/com/example/motiondetectionalert/Gallery.java
+++ AndroidClient/app/src/main/java/com/example/motiondetectionalert/Gallery.java
@@ -2,7 +2,9 @@ package com.example.motiondetectionalert;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -23,9 +25,10 @@ public class Gallery extends AppCompatActivity  {
         setContentView(R.layout.activity_gallery);
 
         this.initializeLayout();
+        this.setListener();
     }
 
-    public void initializeLayout(){
+    private void initializeLayout(){
         back_button = (Button)findViewById(R.id.back_button);
         menu = (TextView)findViewById(R.id.text_menu);
         date_list = (ListView)findViewById(R.id.date_list);
@@ -37,6 +40,15 @@ public class Gallery extends AppCompatActivity  {
         SwitchListAdapter date_adapter = new SwitchListAdapter(this, datefromdb);
 
         date_list.setAdapter(date_adapter);
+    }
+    private void setListener(){
+        back_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent main = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(main);
+            }
+        });
     }
     //게시판 형식으로 db에 저장된 date들 불러오기
     //GalleryDate class 사용
@@ -51,6 +63,3 @@ public class Gallery extends AppCompatActivity  {
 
     }
 }
-
-
-
