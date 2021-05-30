055733433a5a582254f0f65d5754e57c9876e3cd
KKHYeon <46926610+KKHYeon@users.noreply.github.com>
2021-05-30 11:59:16 +0900

    recommit DB_history.py HIstoryAction.py

--- AppServer/historymanage/DB_history.py
+++ AppServer/historymanage/DB_history.py
@@ -6,15 +6,26 @@ class History:
         with open("../auth/firebaseAuth.json") as f:
             config = json.load(f)
         self.firebase = pyrebase.initialize_app(config)
+        self.historydb = self.firebase.database()
+        self.historystorage = self.firebase.storage()
 
-    def filenametoURL(self, filename)
-        url = ""
-        return url
 
-    def deletePhoto(self, filename):
-        url = self.filenametoURL(filename)
-        pass
+    def deletePhoto(self, user_id, date, photo_id):
+        self.historydb.child("photolist").child(user_id).child("dates").child(date).child("urls").child(photo_id).remove
 
-    def getPhoto(self, filename):
-        url = self.filenametoURL(filename)
-        pass
+    def getPhoto(self, user_id, date):
+        photos = self.historydb.child("photolist").child(user_id).child("dates").child(date).child("urls").get()
+        photos = photos.val().values()
+        print(photos)
+        photo_list = []
+        for url in photos:
+            photo_list.append(self.historystorage.child(url).get_url(None))
+        photo_list = tuple(photo_list)
+        print(photo_list)
+
+        return photo_list
+
+    def getDate(self, user_id):
+        date_list = self.historydb.child("photolist").child(user_id).child("dates").get()
+        date_list = tuple(date_list.val().keys())
+        return date_list
--- AppServer/historymanage/HIstoryAction.py
+++ AppServer/historymanage/HIstoryAction.py
@@ -1,21 +1,33 @@
 from flask import Flask
-from historymanage import History
+from historymanage.DB_history import History
 
 app = Flask(__name__)
-historydb = History()
+history = History()
 
-
-@app.route("/")
 class Action:
     def __init__(self):
         self.delete = False
 
-    def selecthistory(self, historyfile):
-        photo = historydb.getPhoto(historyfile)
+    def selecthistory(self, user_id, date):
+        photo = history.getPhoto(user_id, date)
         return photo
 
-    def deletehistory(self, historyfile):
-        historydb.deletePhoto(historyfile)
+    def deletehistory(self, user_id, date, photo_id):
+        history.deletePhoto(user_id, date, photo_id)
+
+    def selectdate(self, user_id):
+        date = history.getDate(user_id)
+        return date
+
+@app.route("/")
+def useraction():
+    # user에게 action과 filename을 받는다
+    # 받은 action을 처리
+    action = Action()
+    #p = action.selecthistory("kang", "2021-05-26")
+    p = action.selectdate("kang")
+    return p
+
 
 
 if __name__ == "__main__":
