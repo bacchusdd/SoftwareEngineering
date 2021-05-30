98a041f1e6d12587b66dd41a77c133268fefba37
bacchusdd <55009516+bacchusdd@users.noreply.github.com>
2021-05-31 02:55:24 +0900

    Update app.py

--- AppServer/app.py
+++ AppServer/app.py
@@ -9,6 +9,17 @@ db = DBModule()
 def hello_world():
     return "Hello AppServer!"
 
+@app.route("/photolist", methods=["GET"])
+def getPhoto_():
+    user_id = request.args.get("user_id")
+    date = request.args.get("date")
+    ph = db.getPhoto(user_id, date)
+
+    if ph:
+        return jsonify(ph), 200
+    else:
+        return jsonify({"isSuccess": False}), 200
+
 
 if __name__ == "__main__":
     app.run(debug=True)
